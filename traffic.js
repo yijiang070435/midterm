8ccc5742e23f121f4d97eec0741e8070ef8ea849
Tianqi0708 <litq0708@outlook.com>
2018-11-08 00:56:52 -0500

    plot

---
+++ traffic.js
@@ -0,0 +1,145 @@
+var svg = d3.select("svg"),
+margin = {top: 20, right: 20, bottom: 30, left: 40},
+width = +svg.attr("width") - margin.left - margin.right,
+height = +svg.attr("height") - margin.top - margin.bottom;
+
+var month="2015-05";
+var type="1";
+var gate="camping0";
+var x = d3.scaleBand().range([0, width]).padding(0.2).round(true);
+//var x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05).align(0.1);
+//var x = d3.scaleOrdinal().range([0, width], .05);
+var y = d3.scaleLinear().rangeRound([height, 0]);
+var z = d3.scaleOrdinal()
+.range(["black", "green", "steelblue", "red", "orange", "purple", "gray"]);
+var data;
+d3.csv("Lekagul_Sensor_Data.csv", function(d, i, columns) {
+  d.Timestamp=d.Timestamp.split(" ")[0];
+  d.month=d.Timestamp.split(" ")[0].substring(0, 7);
+  d.day=d.Timestamp.split(" ")[0].split("-")[2];
+  //console.log(d);
+
+  return d;
+}, function(error, d){data=d;});
+
+
+function drawTraffic(data) {
+  console.log(data);
+  var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  if(type!="all types"){
+    var dataFiltered= data.filter(function (d) { return d.month == month && d["car-type"]==type && d["gate-name"]==gate;});
+    var nested = d3.nest()
+    .key(function(d){return d.Timestamp;})
+    .key(function(d){return d["car-type"];})
+    .key(function(d){return d["gate-name"];})
+    .rollup(function(v) { return v.length; })
+    .entries(dataFiltered);
+    
+    
+    var keys = ["1", "2", "2P", "3", "4", "5", "6"];
+    x.domain(nested.map(function(d) { return d.key.substring(5, 10); }));
+
+    y.domain([0, d3.max(nested, function(d) { return d.values[0].values[0].value; })]);
+    console.log(1);
+    z.domain(keys);
+
+    g.selectAll("bar")
+      .data(nested)
+      .enter().append("rect")
+      .style("fill", "steelblue")
+      .attr("x", function(d) { console.log(d.key.substring(5, 10));return x(d.key.substring(5, 10)); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.values[0].values[0].value); })
+      .attr("height", function(d) { return height - y(d.values[0].values[0].value); });
+  }
+  else{
+    var dataFiltered= data.filter(function (d) { return d.month == month && d["gate-name"]==gate;});
+    var nested = d3.nest()
+    .key(function(d){return d.Timestamp;})
+    .key(function(d){return d["car-type"];})
+    .sortKeys(d3.ascending)
+    .key(function(d){return d["gate-name"];})
+    .rollup(function(v) { return v.length; })
+    .entries(dataFiltered);
+    
+    x.domain(nested.map(function(d) { return d.key.substring(5, 10); }));
+    //y.domain([0, d3.max(nested, function(d) { return d.values[0].values[0].value; })]);
+    
+    var keys = ["1", "2", "2P", "3", "4", "5", "6"];
+    var stackGenerator=d3.stack().keys(keys).value((d, key)=>{
+      
+      
+      var i = d.values.length;
+      while (i--) 
+        if (d.values[i].key == key)  break;
+      
+
+      
+      return i!=-1?  d.values[i].values[0].value : 0;}) (nested);
+    y.domain([0, d3.max(stackGenerator, function(d){return d3.max(d, function(d){return d3.max(d)})})]);
+
+    
+    g.append("g")
+    .selectAll("g")
+    .data(stackGenerator)
+    .enter().append("g")
+    .attr("fill", function(d) { return z(d.key); })
+    .selectAll("rect")
+    .data(function(d) { return d; })
+    .enter().append("rect")
+    .attr("x", function(d) { return x(d.data.key.substring(5, 10)); })
+    .attr("y", function(d) { return y(d[1]); })
+    .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+    .attr("width", x.bandwidth());
+  }
+g.append("g")
+.attr("class", "axis")
+.attr("transform", "translate(0," + height + ")")
+.call(d3.axisBottom(x));
+
+g.append("g")
+.attr("class", "axis")
+.call(d3.axisLeft(y).ticks(null, "s"))
+.append("text")
+.attr("x", 2)
+.attr("y", y(y.ticks().pop()) + 0.5)
+.attr("dy", "0.3em")
+.attr("fill", "black")
+.attr("font-weight", "bold")
+.attr("text-anchor", "start")
+.text("Population");
+
+if(type=="all types")
+{
+  var legend = g.append("g")
+  .attr("font-family", "sans-serif")
+  .attr("font-size", 10)
+  .attr("text-anchor", "end")
+  .selectAll("g")
+  .data(keys.slice().reverse())
+  .enter().append("g")
+  .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+  .attr("x", width - 19)
+  .attr("width", 19)
+  .attr("height", 19)
+  .attr("fill", z);
+
+  legend.append("text")
+  .attr("x", width - 24)
+  .attr("y", 9.5)
+  .attr("dy", "0.32em")
+  .text(function(d) { return d; });
+}
+}
+
+function Click(){
+  month=document.getElementById("sel4").value;
+  type= document.getElementById("sel5").value;
+  gate=document.getElementById("sel6").value;
+  d3.selectAll("g").remove();
+  console.log(month, type, gate);
+  drawTraffic(data);
+}
