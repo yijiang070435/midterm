c2054fbf461bcf5de1714af9b71f833e427d114e
Yi Jiang <yijiang@deepcode.cc>
2018-11-10 01:38:49 -0500

    Update air.js

--- air.js
+++ air.js
@@ -1,57 +1,45 @@
-var width = 1200,
+  
+function drawair(Month){
+  var width = 720,
       height = 720,
       start = 0,
-      end = 2.25,
-      numSpirals = 3
+      end = 2.35,
+      numSpirals = 2
       margin = {top:10,bottom:10,left:10,right:10};
 
-    var theta = function(r) {
-      return numSpirals * Math.PI * r;
-    };
-
-    // used to assign nodes color by group
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-    var r = d3.min([width, height]) / 2 - 40;
-
-    var radius = d3.scaleLinear()
-      .domain([start, end])
-      .range([40, r]);
+    var theta = function(r) {return numSpirals * Math.PI * r;};
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+    var r = d3.min([width, height]) / 2 - 30;
+    var radius = d3.scaleLinear().domain([start, end]).range([30, r]);
 
     var svg = d3.select("#chart").append("svg")
       .attr("width", width + margin.right + margin.left)
-      .attr("height", height + margin.left + margin.right)
-      .append("g")
+      .attr("height", height + margin.left + margin.right).append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     var points = d3.range(start, end + 0.001, (end - start) / 1000);
 
     var spiral = d3.radialLine()
-      .curve(d3.curveCardinal)
-      .angle(theta)
+      .curve(d3.curveCardinal).angle(theta)
       .radius(radius);
 
-    var path = svg.append("path")
-      .datum(points)
-      .attr("id", "spiral")
-      .attr("d", spiral)
-      .style("fill", "none")
-      .style("stroke", "steelblue");
+    var path = svg.append("path").datum(points)
+      .attr("id", "spiral").attr("d", spiral)
+      .style("fill", "none").style("stroke", "steelblue");
 
-      var spiralLength = path.node().getTotalLength(),
+    var someData = [];
+    if(Month==4){
+        var spiralLength = path.node().getTotalLength(),
         N = 270,
         barWidth = (spiralLength / N) - 1;
-        var someData = [];
-    d3.csv("chemical.csv", function(d, i, columns) {
+        
+    d3.csv("chemical1.csv", function(d, i, columns) {
       for (i = 2, t = 0; i < 6; i++) 
-      {
-        t += d[columns[i]] = +d[columns[i]];
-          d.total = t;
-      }    
+      { t += d[columns[i]] = +d[columns[i]];
+          d.total = t;}    
       d.month=d.Date.split(" ")[0].substring(0, 7);
       d.day=d.Date.split(" ")[0].split("/")[2];
-
-      console.log(d.day)
+      d.monitor=+d.monitor;
    
       var currentDate = new Date();
       currentDate.setDate(d.id);
@@ -59,25 +47,22 @@ var width = 1200,
         datetime:d.Date,
         date: currentDate,
         value: d.total,
-        group: d.day
+        monitor:d.Monitor,
+        group: d.day,
+        day:d.day
       });
-    
-     
+        
       return d;
     },function(error,data){
-      
- console.log(someData);
+
     var timeScale = d3.scaleTime()
       .domain(d3.extent(someData, function(d){
-        return d.date;
-      }))
+        return d.date;}))
       .range([0, spiralLength]);
     
-    // yScale for the bar height
     var yScale = d3.scaleLinear()
       .domain([0, d3.max(someData, function(d){
-        return d.value;
-      })])
+        return d.value;})])
       .range([0, (r / numSpirals) - 30]);
 
     d3.select('#chart').selectAll("rect")
@@ -88,34 +73,19 @@ var width = 1200,
         
         var linePer = timeScale(d.date),
             posOnLine = path.node().getPointAtLength(linePer),
-            angleOnLine = path.node().getPointAtLength(linePer - barWidth);
-      
-        d.linePer = linePer; // % distance are on the spiral
-        d.x = posOnLine.x+600; // x postion on the spiral
-        d.y = posOnLine.y+360; // y position on the spiral
-        
-        d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
-
-        return d.x;
-      })
-      .attr("y", function(d){
-        return d.y;
-      })
-      .attr("width", function(d){
-        return barWidth;
-      })
-      .attr("height", function(d){
-        return yScale(d.value);
-      })
+            angleOnLine = path.node().getPointAtLength(linePer - barWidth);     
+        d.linePer = linePer;
+        d.x = posOnLine.x+360; 
+        d.y = posOnLine.y+360;     
+        d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; 
+        return d.x;})
+      .attr("y", function(d){return d.y;})
+      .attr("width", function(d){return barWidth;})
+      .attr("height", function(d){return yScale(d.value);})
       .style("fill", function(d){return color(d.group);})
       .style("stroke", "none")
       .attr("transform", function(d){
-        return "rotate(" + d.a + "," + d.x  + "," + d.y + ")"; // rotate the bar
-      });
-    
-    // add date labels
-    var tF = d3.timeFormat("%b %Y"),
-        firstInMonth = {};
+        return "rotate(" + d.a + "," + d.x  + "," + d.y + ")"; });
 
     d3.select("#chart").selectAll("text")
       .data(someData)
@@ -125,66 +95,177 @@ var width = 1200,
       .style("text-anchor", "start")
       .style("font", "10px arial")
       .append("textPath")
-      // only add for the first of each month
-      .filter(function(d){
-        var sd = tF(d.date);
-        if (!firstInMonth[sd]){
-          firstInMonth[sd] = 1;
-          return true;
-        }
-        return false;
-      })
       .text(function(d){
-        return tF(d.date);
+         if(d.monitor==7 && d.day==30)
+         {
+          return "Date";     
+         }
+         if(d.monitor == 1){
+            return d.day;}    
       })
-      // place text along spiral
       .attr("xlink:href", "#spiral")
       .style("fill", "grey")
       .attr("startOffset", function(d){
-        return ((d.linePer / spiralLength) * 100) + "%";
-      })
-
-
-    var tooltip = d3.select("#chart")
-    .append('div')
-    .attr('class', 'tooltip');
-
-    tooltip.append('div')
-    .attr('class', 'date');
-    tooltip.append('div')
-    .attr('class', 'value');
-
-    svg.selectAll("rect")
-    .on('mouseover', function(d) {
-
-        tooltip.select('.date').html("Date: <b>" + d.date.toDateString() + "</b>");
-        tooltip.select('.value').html("Value: <b>" + Math.round(d.value*100)/100 + "<b>");
-
-        d3.select(this)
-        .style("fill","#FFFFFF")
-        .style("stroke","#000000")
-        .style("stroke-width","2px");
-
-        tooltip.style('display', 'block');
-        tooltip.style('opacity',2);
-
+        return ((d.linePer / spiralLength) * 100) + "%";})
+    d3.select("#chart").selectAll("text")
+      .attr("transform", "translate(" + (360) + "," + ( 360) + ")")
     })
-    .on('mousemove', function(d) {
-        tooltip.style('top', (d3.event.layerY + 10) + 'px')
-        .style('left', (d3.event.layerX - 25) + 'px');
-    })
-    .on('mouseout', function(d) {
-        d3.selectAll("rect")
-        .style("fill", function(d){return color(d.group);})
-        .style("stroke", "none")
+  }
+    if(Month==8){
+        var spiralLength = path.node().getTotalLength(),
+        N = 279,
+        barWidth = (spiralLength / N) - 1;
+        
+    d3.csv("chemical2.csv", function(d, i, columns) {
+      for (i = 2, t = 0; i < 6; i++) 
+      { t += d[columns[i]] = +d[columns[i]];
+          d.total = t;}    
+      d.month=d.Date.split(" ")[0].substring(0, 7);
+      d.day=d.Date.split(" ")[0].split("/")[2];
+      d.monitor=+d.monitor;
+      var currentDate = new Date();
+      currentDate.setDate(d.id);
+      someData.push({
+        datetime:d.Date,
+        date: currentDate,
+        value: d.total,
+        monitor:d.Monitor,
+        group: d.day,
+        day:d.day})       
+      return d;
+    },function(error,data){
 
-        tooltip.style('display', 'none');
-        tooltip.style('opacity',0);
-    });
-
-
-    })
-
+    var timeScale = d3.scaleTime()
+      .domain(d3.extent(someData, function(d){
+        return d.date;}))
+      .range([0, spiralLength]);
     
+    var yScale = d3.scaleLinear()
+      .domain([0, d3.max(someData, function(d){
+        return d.value;})])
+      .range([0, (r / numSpirals) - 30]);
+
+    d3.select('#chart').selectAll("rect")
+      .data(someData)
+      .enter()
+      .append("rect")
+      .attr("x", function(d,i){
+        
+        var linePer = timeScale(d.date),
+            posOnLine = path.node().getPointAtLength(linePer),
+            angleOnLine = path.node().getPointAtLength(linePer - barWidth);     
+        d.linePer = linePer;
+        d.x = posOnLine.x+360; 
+        d.y = posOnLine.y+360;     
+        d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; 
+        return d.x;})
+      .attr("y", function(d){return d.y;})
+      .attr("width", function(d){return barWidth;})
+      .attr("height", function(d){return yScale(d.value);})
+      .style("fill", function(d){return color(d.group);})
+      .style("stroke", "none")
+      .attr("transform", function(d){return "rotate(" + d.a + "," + d.x  + "," + d.y + ")"; });
+
+      d3.select("#chart").selectAll("text")
+      .data(someData)
+      .enter()
+      .append("text")
+      .attr("dy", 10)
+      .style("text-anchor", "start")
+      .style("font", "10px arial")
+      .append("textPath")
+      .text(function(d){
+         if(d.monitor==7 && d.day==31){return "Date";}
+         if(d.monitor == 1){return d.day;}})
+      .attr("xlink:href", "#spiral")
+      .style("fill", "grey")
+      .attr("startOffset", function(d){
+        return ((d.linePer / spiralLength) * 100) + "%";})
+    d3.select("#chart").selectAll("text")
+      .attr("transform", "translate(" + (360) + "," + ( 360) + ")")})}
+
+    if(Month==12){
+      var spiralLength = path.node().getTotalLength(),
+        N = 279,
+        barWidth = (spiralLength / N) - 1;
+        
+    d3.csv("chemical3.csv", function(d, i, columns) {
+      for (i = 2, t = 0; i < 6; i++) 
+      { t += d[columns[i]] = +d[columns[i]];
+          d.total = t;}    
+      d.month=d.Date.split(" ")[0].substring(0, 7);
+      d.day=d.Date.split(" ")[0].split("/")[2];
+      d.monitor=+d.monitor;
+      var currentDate = new Date();
+      currentDate.setDate(d.id);
+      someData.push({
+        datetime:d.Date,
+        date: currentDate,
+        value: d.total,
+        monitor:d.Monitor,
+        group: d.day,
+        day:d.day})
+      return d;
+    },function(error,data){
+
+    var timeScale = d3.scaleTime()
+      .domain(d3.extent(someData, function(d){
+        return d.date;}))
+      .range([0, spiralLength]);
+    
+    var yScale = d3.scaleLinear()
+      .domain([0, d3.max(someData, function(d){
+        return d.value;})])
+      .range([0, (r / numSpirals) - 30]);
+
+    d3.select('#chart').selectAll("rect")
+      .data(someData)
+      .enter()
+      .append("rect")
+      .attr("x", function(d,i){
+        
+      var linePer = timeScale(d.date),
+            posOnLine = path.node().getPointAtLength(linePer),
+            angleOnLine = path.node().getPointAtLength(linePer - barWidth);     
+        d.linePer = linePer;
+        d.x = posOnLine.x+360; 
+        d.y = posOnLine.y+360;     
+        d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; return d.x;})
+      .attr("y", function(d){return d.y;})
+      .attr("width", function(d){return barWidth;})
+      .attr("height", function(d){return yScale(d.value);})
+      .style("fill", function(d){return color(d.group);})
+      .style("stroke", "none")
+      .attr("transform", function(d){
+        return "rotate(" + d.a + "," + d.x  + "," + d.y + ")"; });
+
+    var tF = d3.timeFormat("%b %Y"),firstInMonth = {};
+
+    d3.select("#chart").selectAll("text")
+      .data(someData).enter()
+      .append("text")
+      .attr("dy", 10)
+      .style("text-anchor", "start")
+      .style("font", "10px arial")
+      .append("textPath")
+      .text(function(d){
+         if(d.monitor==7 && d.day==31){return "Date";}
+         if(d.monitor == 1){return d.day;}})
+      .attr("xlink:href", "#spiral")
+      .style("fill", "grey")
+      .attr("startOffset", function(d){
+        return ((d.linePer / spiralLength) * 100) + "%";})
+    d3.select("#chart").selectAll("text")
+      .attr("transform", "translate(" + (360) + "," + ( 360) + ")")})}     
+  }
+
+function CLICK()
+{
+  Month=document.getElementById("sel1").value;
+  d3.select("#chart").selectAll('rect').remove();
+  d3.select("#chart").selectAll('text').remove();
+
+  drawair(Month);
+}
    
     
