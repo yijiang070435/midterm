1e5cba1e95ac031e8e2f7a000fe72be1214bc51e
Tianqi0708 <litq0708@outlook.com>
2018-11-11 11:30:28 -0500

    update

--- traffic.js
+++ traffic.js
@@ -15,6 +15,7 @@ d3.csv("Lekagul_Sensor_Data.csv", function(d, i, columns) {
   d.Timestamp=d.Timestamp.split(" ")[0];
   d.month=d.Timestamp.split(" ")[0].substring(0, 7);
   d.day=d.Timestamp.split(" ")[0].split("-")[2];
+
   //console.log(d);
 
   return d;
@@ -25,89 +26,92 @@ function drawTraffic(data) {
   
   var g = svg.append("g").attr("transform", "translate(" + 60 + "," + 20 + ")");
 
-  if(type!="all types"&&month!=0&&gate!=0){
-    var dataFiltered= data.filter(function (d) { return d.month == month && d["car-type"]==type && d["gate-name"]==gate;});
+//   if(type!="all types"&&month!=0&&gate!=0){
+//     var dataFiltered= data.filter(function (d) { return d.month == month && d["car-type"]==type && d["gate-name"]==gate;});
+//     var nested = d3.nest()
+//     .key(function(d){return d.Timestamp;})
+//     .key(function(d){return d["car-type"];})
+//     .key(function(d){return d["gate-name"];})
+//     .rollup(function(v) { return v.length; })
+//     .entries(dataFiltered);
+    
+//     console.log(nested);
+//     var keys = ["1", "2", "2P", "3", "4", "5", "6"];
+//     var days=["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+//     x.domain(days.map(function(d) { console.log(month.substring(5,7)+"-"+d);return month.substring(5,7)+"-"+d; }));
+   
+//     y.domain([0, d3.max(nested, function(d) { return d.values[0].values[0].value; })+3]);
+   
+//     z.domain(keys);
+
+//     g.selectAll("bar")
+//       .data(nested)
+//       .enter().append("rect")
+//       .style("fill", "steelblue")
+//       .attr("x", function(d) { return x(d.key.substring(5, 10)); })
+//       .attr("width", x.bandwidth())
+//       .attr("y", function(d) { return y(d.values[0].values[0].value); })
+//       .attr("height", function(d) { return height - y(d.values[0].values[0].value); })
+//       .on("mouseover",function(d,i){
+//                 d3.select(this)
+//                 .style('fill','pink')})
+//       .on("mouseleave", function(d){ 
+//                 d3.select(this)
+//                 .style('fill','steelblue')})
+//       .on("mousemove", function(d){
+//                 tooltip
+//                 .style("left", d3.event.pageX - 50 + "px")
+//                 .style("top", d3.event.pageY - 70 + "px")
+//                 .style("display", "inline-block")
+//                 .html(d.key.substring(0, 10)+" : "+d.values[0].values[0].value)})
+//        .on("mouseout", function(d){ 
+//                 tooltip.style("display", "none");})
+//        g.append("g")
+// .attr("class", "axis")
+// .attr("transform", "translate(0," + height + ")")
+// .call(d3.axisBottom(x));
+
+// g.append("g")
+// .attr("class", "axis")
+// .call(d3.axisLeft(y).ticks(null, "s"))
+// .append("text")
+// .attr("x", 2)
+// .attr("y", y(y.ticks().pop()) + 0.5)
+// .attr("dy", "0.3em")
+// .attr("fill", "black")
+// .attr("font-weight", "bold")
+// .attr("text-anchor", "start")
+// .text("Car Number");
+//   }
+  
+    var keys = ["1", "2", "2P", "3", "4", "5", "6"];
+    var days=["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+    var dataFiltered= data.filter(function (d) { return d.month == month;});
     var nested = d3.nest()
     .key(function(d){return d.Timestamp;})
-    .key(function(d){return d["car-type"];})
-    .key(function(d){return d["gate-name"];})
-    .rollup(function(v) { return v.length; })
+    .key(function(d){ return d["car-type"];})
+    .sortKeys(d3.ascending)
+    //.key(function(d){return d["gate-name"];})
+    //.rollup(function(v) { return {total: v.length}; })
     .entries(dataFiltered);
     
     console.log(nested);
-    var keys = ["1", "2", "2P", "3", "4", "5", "6"];
-    var days=["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
-    x.domain(days.map(function(d) { console.log(month.substring(5,7)+"-"+d);return month.substring(5,7)+"-"+d; }));
-   
-    y.domain([0, d3.max(nested, function(d) { return d.values[0].values[0].value; })+3]);
-   
-    z.domain(keys);
 
-    g.selectAll("bar")
-      .data(nested)
-      .enter().append("rect")
-      .style("fill", "steelblue")
-      .attr("x", function(d) { return x(d.key.substring(5, 10)); })
-      .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.values[0].values[0].value); })
-      .attr("height", function(d) { return height - y(d.values[0].values[0].value); })
-      .on("mouseover",function(d,i){
-                d3.select(this)
-                .style('fill','pink')})
-      .on("mouseleave", function(d){ 
-                d3.select(this)
-                .style('fill','steelblue')})
-      .on("mousemove", function(d){
-                tooltip
-                .style("left", d3.event.pageX - 50 + "px")
-                .style("top", d3.event.pageY - 70 + "px")
-                .style("display", "inline-block")
-                .html(d.key.substring(0, 10)+" : "+d.values[0].values[0].value)})
-       .on("mouseout", function(d){ 
-                tooltip.style("display", "none");})
-       g.append("g")
-.attr("class", "axis")
-.attr("transform", "translate(0," + height + ")")
-.call(d3.axisBottom(x));
-
-g.append("g")
-.attr("class", "axis")
-.call(d3.axisLeft(y).ticks(null, "s"))
-.append("text")
-.attr("x", 2)
-.attr("y", y(y.ticks().pop()) + 0.5)
-.attr("dy", "0.3em")
-.attr("fill", "black")
-.attr("font-weight", "bold")
-.attr("text-anchor", "start")
-.text("Car Number");
-  }
-  if(type=="all types"&&month!=0&&gate!=0){
-    var keys = ["1", "2", "2P", "3", "4", "5", "6"];
-    var days=["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
-    var dataFiltered= data.filter(function (d) { return d.month == month && d["gate-name"]==gate;});
-    var nested = d3.nest()
-    .key(function(d){return d.Timestamp;})
-    .key(function(d){return d["car-type"];})
-    .sortKeys(d3.ascending)
-    .key(function(d){return d["gate-name"];})
-    .rollup(function(v) { return v.length; })
-    .entries(dataFiltered);
-    
-    x.domain(days.map(function(d) { console.log(month.substring(5,7)+"-"+d);return month.substring(5,7)+"-"+d; }));
-    //y.domain([0, d3.max(nested, function(d) { return d.values[0].values[0].value; })]);
+    x.domain(days.map(function(d) { return month.substring(5,7)+"-"+d; }));
     
 
     var stackGenerator=d3.stack().keys(keys).value((d, key)=>{
       
-      
+      console.log(d);
       var i = d.values.length;
       while (i--) 
         if (d.values[i].key == key)  break;
       
 
-      
-      return i!=-1?  d.values[i].values[0].value : 0;}) (nested);
+      if(i!=-1){
+        console.log(d.values[i].values.length);
+      }
+      return i!=-1?  d.values[i].values.length : 0;}) (nested);
     y.domain([0, d3.max(stackGenerator, function(d){return d3.max(d, function(d){return d3.max(d)})})]);
 
     
@@ -147,19 +151,14 @@ g.append("g")
 .attr("class", "axis")
 .call(d3.axisLeft(y).ticks(null, "s"))
 .append("text")
-.attr("x", 2)
-.attr("y", y(y.ticks().pop()) + 0.5)
+.attr("x", 1)
+.attr("y", y(y.ticks().pop()))
 .attr("dy", "0.3em")
 .attr("fill", "black")
 .attr("font-weight", "bold")
 .attr("text-anchor", "start")
 .text("Car Number");
-    
 
-  }
-
-if(type=="all types")
-{
   var legend = g.append("g")
   .attr("font-family", "sans-serif")
   .attr("font-size", 10)
@@ -180,14 +179,14 @@ if(type=="all types")
   .attr("y", 9.5)
   .attr("dy", "0.32em")
   .text(function(d) { return d; });
-}
+
 }
 
 function Click(){
   month=document.getElementById("sel4").value;
-  type= document.getElementById("sel5").value;
-  gate=document.getElementById("sel6").value;
+  //type= document.getElementById("sel5").value;
+  //gate=document.getElementById("sel6").value;
   d3.select("#bar").selectAll('g').remove();
-  console.log(month, type, gate);
+  console.log(month);
   drawTraffic(data);
 }
