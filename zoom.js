08c457be13365196d6224b51b6780656916d6bc9
wxual <44627587+wxual@users.noreply.github.com>
2018-11-10 22:13:12 -0500

    Update zoom.js

--- zoom.js
+++ zoom.js
@@ -1,7 +1,8 @@
 function imageZoom(imgID, resultID) {
-  var img, lens, result, cx, cy;
+  var img, lens, result, cx, cy, a;
   img = document.getElementById(imgID);
   result = document.getElementById(resultID);
+  a = img.getBoundingClientRect();
   /*create lens:*/
   lens = document.createElement("DIV");
   lens.setAttribute("class", "img-zoom-lens");
@@ -10,18 +11,16 @@ function imageZoom(imgID, resultID) {
   /*calculate the ratio between result DIV and lens:*/
   cx = result.offsetWidth / lens.offsetWidth;
   cy = result.offsetHeight / lens.offsetHeight;
-  /*set background properties for the result DIV*/
+  /*set background properties for the result DIV:*/
   result.style.backgroundImage = "url('" + img.src + "')";
   result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
   /*execute a function when someone moves the cursor over the image, or the lens:*/
   lens.addEventListener("mousemove", moveLens);
   img.addEventListener("mousemove", moveLens);
-  /*and also for touch screens:*/
-  lens.addEventListener("touchmove", moveLens);
-  img.addEventListener("touchmove", moveLens);
+
   function moveLens(e) {
     var pos, x, y;
-    /*prevent any other actions that may occur when moving over the image*/
+    /*prevent any other actions that may occur when moving over the image:*/
     e.preventDefault();
     /*get the cursor's x and y positions:*/
     pos = getCursorPos(e);
@@ -35,7 +34,7 @@ function imageZoom(imgID, resultID) {
     if (y < 0) {y = 0;}
     /*set the position of the lens:*/
     lens.style.left = x + "px";
-    lens.style.top = y + "px";
+    lens.style.top = y +105+ "px";
     /*display what the lens "sees":*/
     result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
   }
@@ -53,3 +52,4 @@ function imageZoom(imgID, resultID) {
     return {x : x, y : y};
   }
 }
+ imageZoom("myimage", "myresult");
